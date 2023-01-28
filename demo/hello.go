71d4adbaf64e3e6ccd82d576b793d67c7a628e34
Elucian Moise <elucian.moise@gmail.com>
2023-01-28 07:44:25 -0600

    improve expressions

--- demo/hello.go
+++ demo/hello.go
@@ -1,9 +1,15 @@
 package demo
 
+var message string = "Hello Wordl"
+
 import (
 	"fmt"
 )
 
 func Hello() {
-	fmt.Println("Hello, Hello!")
+	fmt.Println(message)
+	fmt.Println('-' * 10)
+	fmt.Println("Go \n" + 
+	            "is \n" +
+				"ossome \n")	
 }
